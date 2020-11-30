7d0e939f3421ac183d6cec763b06e680c19b6602
kdaud <dkakumirizii@gmail.com>
2020-11-30 11:09:24 +0300

    React api confluence

--- javatech/commiter.js
+++ javatech/commiter.js
@@ -7,10 +7,15 @@ class Committer{
     getCommiterInfo(){
         return `${this.username} volunteers with ${this.community}`;
     }
+
+    getCoredev(){
+        return "I want to be like dkayiwa someday!"
+    }
 }
 
 class CoreCommiter extends Committer{
 
 }
  const kdaud = new CoreCommiter('kdaud','OpenMRS');
- console.log(kdaud.getCommiterInfo);
+ console.log(kdaud.getCommiterInfo());
+ console.log(kdaud.getCoredev());
